9cd1b21c176e9512cf7ac20071a751d07cdc5962
slkjayy93 <74787049+slkjayy93@users.noreply.github.com>
2023-12-01 22:40:24 +0100

    feat: Partie 8, Prometheus FINI

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -1,28 +1,41 @@
-const express = require('express')
-const userRouter = require('./routes/user')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/user');
+const db = require('./dbClient');
+const promClient = require('prom-client');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-const db = require('./dbClient')
+// Création d'un collecteur de métriques par défaut
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+
+// Collecte des métriques toutes les 5 secondes
+collectDefaultMetrics({ timeout: 5000 });
+
+// Configuration du middleware body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Point de terminaison pour les métriques de Prometheus
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+});
+
+// Autres routes de l'application
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/user', userRouter);
+
+// Gestion des erreurs de connexion à la base de données
 db.on("error", (err) => {
-  console.error(err)
-})
+    console.error(err);
+});
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.use('/user', userRouter)
-
+// Démarrage du serveur
 const server = app.listen(port, (err) => {
-  if (err) throw err
-  console.log("Server listening the port " + port)
-})
+    if (err) throw err;
+    console.log("Server listening the port " + port);
+});
 
-
-module.exports = server
+module.exports = server;
